085c1e049038d55190d474ce8bb9918f6edaee51
Adam Rosenbaum <adam.rosenbaum@scilifelab.se>
2019-10-17 09:10:38 +0200

    More tests

--- tests/utils/test_write_report.py
+++ tests/utils/test_write_report.py
@@ -5,3 +5,10 @@ def test_instatiate(intersection, compare_fields):
 
     writer = ReportWriter(intersection, compare_fields)
     assert isinstance(writer.aggr_stats.stats, dict)
+
+def test_write(intersection, compare_fields):
+
+    writer = ReportWriter(intersection, compare_fields)
+
+    for record in writer.write():
+        assert len(str(record).split('\n')) > 1
